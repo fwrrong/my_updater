aeb26456dbc25107a5806d217e87b0c36835cabc
fwrrong <fwrrong@umich.edu>
2023-11-29 13:14:02 -0500

    init commit

---
+++ src/test/java/com/fwrrong/my_updater/MyUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwrrong.my_updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUpdaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
